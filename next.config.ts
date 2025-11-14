0f7783830e27ebdb76020540234fac17b34a09a4
kittenwarrior-qb <kittenwarrior2005@gmail.com>
2025-11-14 09:52:59 +0700

    fix: middleware route

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
-import path from 'path';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '../'),
   images: {
     remotePatterns: [
       {
